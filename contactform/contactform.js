4d6c6ce024b2e3ae9efb3e8e2e4c23c12a248b05
talagart <56836121+talagart@users.noreply.github.com>
2024-03-20 16:16:41 +0000

    cloud integration

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -1,25 +1,35 @@
-$(document).ready(function() {
-  $('.contactForm').submit(function(e) {
-    e.preventDefault(); // Zapobiegaj domyślnemu zachowaniu formularza (czyli przekierowaniu)
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Zapobiegaj domyślnemu zachowaniu formularza (czyli przekierowaniu)
+    
+    // Pobierz dane z formularza
+    var formData = new FormData(this);
 
-    var form = $(this);
-    var formData = form.serialize(); // Pobierz dane z formularza
+    // Ustaw adres URL interfejsu API Gateway
+    var apiUrl = 'https://or1tlywqjj.execute-api.us-east-1.amazonaws.com/prod';
 
-    // Wykonaj żądanie AJAX
-    $.ajax({
-      type: 'POST',
-      url: 'contactform/contactform.php', // Adres URL, do którego wysyłamy dane formularza
-      data: formData, // Dane formularza
-      success: function(response) {
-        if (response == 'OK') {
-          $('#sendmessage').addClass('show');
-          $('#errormessage').removeClass('show');
-          $('.contactForm').find('input, textarea').val('');
-        } else {
-          $('#sendmessage').removeClass('show');
-          $('#errormessage').addClass('show').html(response);
-        }
+    // Wyślij żądanie POST do interfejsu API Gateway
+    fetch(apiUrl, {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => {
+      if (response.ok) {
+        // Jeśli żądanie zostało zakończone sukcesem, wyczyść formularz i wyświetl komunikat sukcesu
+        document.getElementById('sendmessage').classList.add('show');
+        document.getElementById('errormessage').classList.remove('show');
+        document.getElementById('contactForm').reset(); // Wyczyść formularz
+      } else {
+        // Jeśli wystąpił błąd, wyświetl komunikat o błędzie
+        document.getElementById('sendmessage').classList.remove('show');
+        document.getElementById('errormessage').classList.add('show').textContent = 'Oops! Something went wrong. Please try again later.';
       }
+    })
+    .catch(error => {
+      // W przypadku błędu sieciowego, wyświetl komunikat o błędzie
+      document.getElementById('sendmessage').classList.remove('show');
+      document.getElementById('errormessage').classList.add('show').textContent = 'Oops! Something went wrong. Please check your network connection and try again.';
     });
   });
 });
+
