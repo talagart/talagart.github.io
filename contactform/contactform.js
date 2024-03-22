9ac264ac514e3204445407c56239f6675d5edd64
talagart <56836121+talagart@users.noreply.github.com>
2024-03-22 12:59:57 +0000

    prod

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -3,7 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
     event.preventDefault(); // Zapobiegaj domyślnemu zachowaniu formularza (czyli przekierowaniu)
     
     // Pobierz dane z formularza
-    var formData = new FormData(this);
+    var formData = {
+      name: document.getElementById('name').value,
+      email: document.getElementById('email').value,
+      subject: document.getElementById('subject').value,
+      message: document.getElementById('message').value
+    };
 
     // Ustaw adres URL interfejsu API Gateway
     var apiUrl = 'https://tgsvt1e7vl.execute-api.us-east-1.amazonaws.com/prod/formularz';
@@ -11,32 +16,27 @@ document.addEventListener('DOMContentLoaded', function() {
     // Wyślij żądanie POST do interfejsu API Gateway
     fetch(apiUrl, {
       method: 'POST',
-      body: formData
+      body: JSON.stringify(formData), // Przekształć dane na format JSON
+      headers: {
+        'Content-Type': 'application/json' // Ustaw typ zawartości
+      }
     })
     .then(response => {
       if (response.ok) {
         // Jeśli żądanie zostało zakończone sukcesem, wyczyść formularz i wyświetl komunikat sukcesu
-        var sendmessage = document.getElementById('sendmessage');
-        var errormessage = document.getElementById('errormessage');
-        if (sendmessage) sendmessage.classList.add('show');
-        if (errormessage) errormessage.classList.remove('show');
+        document.getElementById('sendmessage').classList.add('show');
+        document.getElementById('errormessage').classList.remove('show');
         document.getElementById('contactForm').reset(); // Wyczyść formularz
       } else {
         // Jeśli wystąpił błąd, wyświetl komunikat o błędzie
-        var errormessage = document.getElementById('errormessage');
-        if (errormessage) {
-          errormessage.textContent = 'Oops! Something went wrong. Please try again later.';
-          errormessage.classList.add('show');
-        }
+        document.getElementById('sendmessage').classList.remove('show');
+        document.getElementById('errormessage').classList.add('show').textContent = 'Oops! Something went wrong. Please try again later.';
       }
     })
     .catch(error => {
       // W przypadku błędu sieciowego, wyświetl komunikat o błędzie
-      var errormessage = document.getElementById('errormessage');
-      if (errormessage) {
-        errormessage.textContent = 'Oops! Something went wrong. Please check your network connection and try again.';
-        errormessage.classList.add('show');
-      }
+      document.getElementById('sendmessage').classList.remove('show');
+      document.getElementById('errormessage').classList.add('show').textContent = 'Oops! Something went wrong. Please check your network connection and try again.';
     });
   });
 });
